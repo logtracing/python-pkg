0c6b5cde46dbc3c0ac8ccf4200086cc42ad9ab32
Rene Barrera <rene.barrera1022@gmail.com>
2023-06-09 13:28:31 -0600

    add Types and base Logger class

--- src/logtracing/Logger.py
+++ src/logtracing/Logger.py
@@ -1,1 +1,20 @@
-# TODO create Logger class
+import os
+import traceback
+from Types import PrepareStackTrace, CodeLine, ErrorStack, OsVars, PythonVars
+from typing import Union
+
+
+class Logger:
+    def __init__(self, flow: str):
+      self._flow = flow;
+      self.prepare_stack_trace: PrepareStackTrace = traceback.extract_stack
+      self.err_stack = None;
+      self.os_vars: Union[OsVars, None] = None;
+      self.python_vars: Union[PythonVars, None] = None;
+      self.env_vars: Union[os.environ, None] = None;
+      self.extra_vars = {};
+      self.code_lines_limit: int = 5;
+
+    @property
+    def flow(self):
+        return self._flow
---
+++ src/logtracing/__init__.py
@@ -0,0 +1,3 @@
+from Logger import Logger
+
+__all__ = ['Logger']
