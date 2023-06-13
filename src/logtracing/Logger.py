69c0dc7a324fdd27c04615c12b0a584523ff0946
Rene Barrera <rene.barrera1022@gmail.com>
2023-06-13 10:17:00 -0600

    validate add_extra

--- src/logtracing/Logger.py
+++ src/logtracing/Logger.py
@@ -1,18 +1,22 @@
 import os
 import sys
+import json
 import time
 import psutil
 import platform
 import traceback
 from typing import Union
-from Types import PrepareStackTrace, CodeLine, ErrorStack, OsVars, PythonVars
+from Types import CodeLine, ErrorStack, PythonVars, OsVars, PrepareStackTrace
 
 
 class Logger:
     def __init__(self, flow: str):
+        if not flow:
+           raise Exception('Flow argument is missing')
+
         self._flow = flow
         self.prepare_stack_trace: PrepareStackTrace = traceback.extract_stack
-        self.err_stack = None
+        self.err_stack: Union[ErrorStack, None] = None
         self.os_vars: Union[OsVars, None] = None
         self.python_vars: Union[PythonVars, None] = None
         self.env_vars: Union[os.environ, None] = None
@@ -24,12 +28,25 @@ class Logger:
         return self._flow
 
     def track_error(self, err) -> None:
-        pass
+        if not err:
+           raise Exception('Error argument is missing')
 
     def report(self) -> None:
-        pass
+        self.load_os_vars()
+        self.load_python_vars()
+        self.load_env_vars()
+
+        print(self.err_stack)
+        print(self.os_vars)
+        print(self.env_vars)
+        print(self.python_vars)
+        print(self.extra_vars)
 
     def add_extra(self, identifier: str, extra) -> None:
+        if type(extra) != dict and type(extra) != str:
+           raise Exception('extra must be dict or str')
+        
+        extra = json.dumps(extra)
         self.extra_vars[identifier] = extra
 
     def read_line(self, file_path: str, start: int, end: int) -> CodeLine:
@@ -87,3 +104,20 @@ class Logger:
 
     def load_env_vars(self) -> None:
         self.env_vars = os.environ
+
+
+if __name__ == '__main__':
+    logger = Logger('OH')
+    logger.add_extra('test_extra_var', {
+        "algo": 0
+    })
+
+    try:
+        raise Exception("Mensaje de error")
+    except Exception as e:
+      logger.add_extra('one_more_extra_vars', {
+          "test": 1
+      })
+
+      logger.track_error('')
+      logger.report()
--- src/logtracing/Types.py
+++ src/logtracing/Types.py
@@ -1,65 +1,69 @@
 import traceback
 from typing import List, Union
 
+
 class PrepareStackTrace:
-  def __init__(self, prepare_stack_trace: Union[BaseException, List[traceback.FrameSummary], None]):
-    self.prepare_stack_trace =  prepare_stack_trace
+    def __init__(self, prepare_stack_trace: Union[BaseException, List[traceback.FrameSummary], None]):
+        self.prepare_stack_trace = prepare_stack_trace
 
 
 class CodeLine:
-  def __init__(self, line: int, content: str, current_line: Union[int, None]):
-    self.line = line
-    self.content = content
-    self.current_line = current_line
+    def __init__(self, line: int, content: str, current_line: Union[int, None]):
+        self.line = line
+        self.content = content
+        self.current_line = current_line
 
 
 class ErrorStack:
-  def __init__(self, err_message: str, function_name: str, file_name: str, line_number: int, column_number: int, code: List[CodeLine]):
-    self.err_message = err_message,
-    self.function_name = function_name
-    self.file_name = file_name
-    self.line_number = line_number
-    self.column_number = column_number
-    self.code = code
+    def __init__(self, error_name: str, error_message: str, error_stack: str, function_name: str, file_name: str, line_number: int, column_number: int, code: List[CodeLine]):
+        self.error_name = error_name
+        self.error_message = error_message
+        self.error_stack = error_stack
+        self.function_name = function_name
+        self.file_name = file_name
+        self.line_number = line_number
+        self.column_number = column_number
+        self.code = code
+
 
 
 class OsUser:
-  def __init__(self, username: str, uid: int, gid: int):
-    self.username = username,
-    self.uid = uid
-    self.gid = gid
+    def __init__(self, username: str, uid: int, gid: int):
+        self.username = username,
+        self.uid = uid
+        self.gid = gid
 
 
 class OsCpuTime:
-  def __init__(self, user: int, nice: int, sys: int, idle: int, irq: int):
-    self.user = user
-    self.nice = nice
-    self.sys = sys
-    self.idle = idle
-    self.irq = irq
+    def __init__(self, user: int, nice: int, sys: int, idle: int, irq: int):
+        self.user = user
+        self.nice = nice
+        self.sys = sys
+        self.idle = idle
+        self.irq = irq
 
 
 class OsCpu:
-  def __init__(self, model: str, speed: int, times: OsCpuTime):
-    self.model = model
-    self.speed = speed
-    self.times = times
+    def __init__(self, model: str, speed: int, times: OsCpuTime):
+        self.model = model
+        self.speed = speed
+        self.times = times
 
 
 class OsVars:
-  def __init__(self, arch: str, cpus: List[OsCpu], hostname: str, machine: str, platform: str, release: str, version: str, user: OsUser):
-    self.arch = arch
-    self.cpus = cpus
-    self.hostname = hostname
-    self.machine = machine
-    self.platform = platform
-    self.release = release
-    self.version = version
-    self.user = user
+    def __init__(self, arch: str, cpus: List[OsCpu], hostname: str, machine: str, platform: str, release: str, version: str, user: OsUser):
+        self.arch = arch
+        self.cpus = cpus
+        self.hostname = hostname
+        self.machine = machine
+        self.platform = platform
+        self.release = release
+        self.version = version
+        self.user = user
 
 
 class PythonVars:
-  def __init__(self, version: str, args: List[str], datetime: int):
-    self.version = version
-    self.args = args
-    self.datetime = datetime
+    def __init__(self, version: str, args: List[str], datetime: int):
+        self.version = version
+        self.args = args
+        self.datetime = datetime
