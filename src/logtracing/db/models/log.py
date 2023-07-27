a6b7063022669817a26e0112ffd8f79418c17b11
renebarrera1022 <rene.barrera1022@gmail.com>
2023-07-27 09:19:59 -0600

    complement contrainst

--- src/logtracing/db/models/log.py
+++ src/logtracing/db/models/log.py
@@ -1,6 +1,6 @@
 from main import BaseModel
 from log_group import LogGroups
-from peewee import SQL, TextField, DateTimeField, CharField, ForeignKeyField
+from peewee import SQL, Check, TextField, DateTimeField, CharField, ForeignKeyField
 
 
 class UnknownField(object):
@@ -11,7 +11,7 @@ class Logs(BaseModel):
     content = TextField(null=True)
     created_at = DateTimeField(column_name='createdAt')
     flow = CharField(null=True)
-    level = CharField(constraints=[SQL("DEFAULT 'INFO'")], null=True)
+    level = CharField(constraints=[Check("level IN ('TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL')")], default='INFO', null=True)
     log_group = ForeignKeyField(column_name='logGroupId', field='id', model=LogGroups, null=True)
     updated_at = DateTimeField(column_name='updatedAt')
 
