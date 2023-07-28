66b576fc4c409fa5c88d3a901d881ba1b945e756
renebarrera1022 <rene.barrera1022@gmail.com>
2023-07-28 11:08:12 -0600

    organize line codes

--- src/logtracing/db/models/system_details.py
+++ src/logtracing/db/models/system_details.py
@@ -9,13 +9,13 @@ class UnknownField(object):
 
 class SystemDetails(BaseModel):
     arch = CharField(null=True)
-    error_exception = ForeignKeyField(column_name='errorExceptionId', field='id', model=ErrorException, null=True)
+    user = CharField(null=True)
     hostname = CharField(null=True)
     platform = CharField(null=True)
+    processor = CharField(null=True)
     platform_release = CharField(column_name='platformRelease', null=True)
     platform_version = CharField(column_name='platformVersion', null=True)
-    processor = CharField(null=True)
-    user = CharField(null=True)
+    error_exception = ForeignKeyField(column_name='errorExceptionId', field='id', model=ErrorException, null=True)
 
     class Meta:
         table_name = 'systemDetails'
