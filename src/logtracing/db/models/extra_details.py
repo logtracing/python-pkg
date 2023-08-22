9b81bb78e1508aaaebb864106e2c9adae1394df7
Mike <popeye.mike13@gmail.com>
2023-08-22 11:34:00 -0600

    :zap: add BooleanField peewee standard

--- src/logtracing/db/models/extra_details.py
+++ src/logtracing/db/models/extra_details.py
@@ -1,6 +1,6 @@
 from main import BaseModel
 from error_exception import ErrorException
-from peewee import IntegerField, CharField, ForeignKeyField
+from peewee import BooleanField, CharField, ForeignKeyField
 
 
 class UnknownField(object):
@@ -10,7 +10,7 @@ class UnknownField(object):
 class ExtraDetails(BaseModel):
     name = CharField(null=True)
     value = CharField(null=True)
-    is_json = IntegerField(column_name='isJson', null=True)
+    is_json = BooleanField(column_name='isJson', null=True)
     error_exception = ForeignKeyField(column_name='errorExceptionId', field='id', model=ErrorException, null=True)
 
     class Meta:
